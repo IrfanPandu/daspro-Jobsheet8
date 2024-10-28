a49bd5c51fa18c4fa507d0c79b2e492f80e91429
Irfan Pandu Pratama <irfanpanduutama@gmail.com>
2024-10-28 22:35:57 +0700

    jobsheet8

--- Triangle13.java
+++ Triangle13.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class Triangle13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nilai N = ");
-        int N = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) { 
-            for (int j = 1; j <= i; j++) { 
+        System.out.print("masukkan nilai N = ");
+        int N = sc.nextInt();
+        int i = 1;
+        while (i <= N) {
+            int j = 0;
+            while ( j < i) {
                 System.out.print("*");
+                j++;
             }
-            System.out.println(); 
+            i++;
+        System.out.println();
+        }
     }
-}
 }
